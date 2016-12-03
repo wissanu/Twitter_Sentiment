f6facf70dfd3b98c5d565493630494680d47eaf5
wissanu <thungrut@gmail.com>
2016-12-03 16:26:38 +0700

    added prediction

--- twitt_test/src/twitt_test/Evaluate.java
+++ twitt_test/src/twitt_test/Evaluate.java
@@ -8,6 +8,8 @@ import com.aliasi.classify.BaseClassifier;
 import com.aliasi.classify.BaseClassifierEvaluator;
 import com.aliasi.classify.Classification;
 import com.aliasi.classify.Classified;
+import com.aliasi.classify.JointClassifier;
+import com.aliasi.classify.JointClassifierEvaluator;
 import com.aliasi.corpus.XValidatingObjectCorpus;
 import com.aliasi.util.AbstractExternalizable;
 
@@ -18,18 +20,17 @@ public class Evaluate {
 		String[] categories = new String[category.size()];
 		for (int i = 0; i < category.size(); i++)
 			categories[i] = category.get(i);
-		BaseClassifier<CharSequence> classifier = (BaseClassifier<CharSequence>) AbstractExternalizable.readObject(new File("model/restaurant.naive_bayes"));
+        JointClassifierEvaluator<CharSequence> classifier = (JointClassifierEvaluator<CharSequence>) AbstractExternalizable.readObject(new File("model/restaurant.naive_bayes"));
 		boolean storeInputs = false;
-		BaseClassifierEvaluator<CharSequence> evaluator 
-			= new BaseClassifierEvaluator<CharSequence>(classifier,categories, storeInputs);
+		JointClassifierEvaluator<CharSequence> evaluator = new JointClassifierEvaluator<CharSequence>((JointClassifier<CharSequence>) classifier,categories, storeInputs);
 		for (int i = 0; i < data.size(); i++) {
 			String temp_categories = categories[i];
 			Classification classification = new Classification(temp_categories);
 			Classified<CharSequence> classified = new Classified<CharSequence>(data.get(i),classification);
 			evaluator.handle(classified);
 		}
-		//Util.printConfusionMatrix(evaluator.confusionMatrix());
-		System.out.println(evaluator.confusionMatrix());
+		Util.printConfusionMatrix(evaluator.confusionMatrix());
+		//System.out.println(evaluator.toString());
 		//Util.printPrecRecall(evaluator);
 	}
 }
